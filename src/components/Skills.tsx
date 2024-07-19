53b4be125166d44f1181b06e980150f521da3bb6
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2024-07-19 21:45:01 +0100

    More tools added

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,8 +2,10 @@ import {
   SiMicrosoftexcel,
   SiMicrosoftpowerpoint,
   SiMicrosoftsqlserver,
+  SiMicrosoftword,
   SiMysql,
   SiPowerbi,
+  SiSage,
   SiTableau,
 } from "react-icons/si";
 
@@ -24,34 +26,48 @@ const Skills: React.FC = () => {
           </div>
 
           <div className="flex flex-wrap gap-12 justify-center items-center">
-            <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#1757b7]">
+              <SiMicrosoftword className="text-5xl" />
+              <p className="text-sm text-center">Microsoft Word</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#0e783f]">
               <SiMicrosoftexcel className="text-5xl" />
-              <p className="text-xs text-center">Microsoft Excel</p>
+              <p className="text-sm text-center">Microsoft Excel</p>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4">
-              <SiMicrosoftsqlserver className="text-5xl" />
-              <p className="text-xs text-center">Microsoft SSMS</p>
-            </div>
-
-            <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#ce4f2e]">
               <SiMicrosoftpowerpoint className="text-5xl" />
-              <p className="text-xs text-center">Microsoft Powerpoint</p>
+              <p className="text-sm text-center">Microsoft Powerpoint</p>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-slate-500">
+              <SiMicrosoftsqlserver className="text-5xl" />
+              <p className="text-sm text-center">Microsoft SSMS</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#005b85]">
               <SiMysql className="text-5xl" />
-              <p className="text-xs text-center">MySQL</p>
+              <p className="text-sm text-center">MySQL</p>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#efc42f]">
               <SiPowerbi className="text-5xl" />
-              <p className="text-xs text-center">PowerBi</p>
+              <p className="text-sm text-center">PowerBi</p>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4">
-              <SiTableau className="text-5xl" />
-              <p className="text-xs text-center">Tableau</p>
+            <div className="
+              flex flex-col justify-center items-center gap-4 
+              hover:bg-gradient-to-tr from-[#d51b35] via-[#ec6e2a] to-[#5087a6]
+              bg-clip-text text-fill-none group
+            ">
+              <SiTableau className="text-5xl group-hover:text-[#d51b35]" />
+              <p className="text-sm text-center">Tableau</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 hover:text-[#0ad639]">
+              <SiSage className="text-5xl" />
+              <p className="text-sm text-center">Sage</p>
             </div>
           </div>
         </div>
